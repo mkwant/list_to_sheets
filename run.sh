cf2318be622ac1e2e5a09e5282c25bf4c367e834
Maarten Kwant <maarten.kwant@nepgroup.com>
2024-01-05 14:58:35 +0100

    Added .env-file

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 arguments=( "$@" )
 
 cd /var/lib/list_to_sheets || exit
-/bin/docker run --rm -t -v "${PWD}"/client_secrets.json:/app/src/client_secrets.json -v "${PWD}"/mycreds.json:/app/src/mycreds.json list_to_sheets "${arguments[@]}"
+/bin/docker run --rm -t --env-file .env -v "${PWD}"/client_secrets.json:/app/src/client_secrets.json -v "${PWD}"/mycreds.json:/app/src/mycreds.json list_to_sheets "${arguments[@]}"
