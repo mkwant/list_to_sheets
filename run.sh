dba41331e2f6921b5942709554a36da398156645
Maarten Kwant <maarten.kwant@nepgroup.com>
2024-01-05 15:37:52 +0100

    Removed test code

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 arguments=( "$@" )
 
 cd /var/lib/list_to_sheets || exit
-/bin/docker run -it --env-file .env -v /var/log/list_to_sheets:/log -v "${PWD}"/client_secrets.json:/app/src/client_secrets.json -v "${PWD}"/mycreds.json:/app/src/mycreds.json list_to_sheets "${arguments[@]}"
-#/bin/docker run --rm -t --env-file .env -v /var/log/list_to_sheets:/log -v "${PWD}"/client_secrets.json:/app/src/client_secrets.json -v "${PWD}"/mycreds.json:/app/src/mycreds.json list_to_sheets "${arguments[@]}"
+/bin/docker run --rm -t --env-file .env -v /var/log/list_to_sheets:/log -v "${PWD}"/client_secrets.json:/app/src/client_secrets.json -v "${PWD}"/mycreds.json:/app/src/mycreds.json list_to_sheets "${arguments[@]}"
