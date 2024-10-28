db8aa19387440705027f0f0c2221118c2901e4a2
Maarten Kwant <maarten.kwant@nepgroup.com>
2024-10-28 13:48:16 +0100

    Point towards ghcr docker image

--- run.sh
+++ run.sh
@@ -2,4 +2,9 @@
 arguments=( "$@" )
 
 cd /var/lib/list_to_sheets || exit
-/bin/docker run --rm -t --env-file .env -v /var/log/list_to_sheets:/log -v "${PWD}"/client_secrets.json:/app/src/client_secrets.json -v "${PWD}"/mycreds.json:/app/src/mycreds.json list_to_sheets "${arguments[@]}"
+/bin/docker run --rm -t \
+            --env-file .env \
+            -v /var/log/list_to_sheets:/log \
+            -v "${PWD}"/client_secrets.json:/app/src/client_secrets.json \
+            -v "${PWD}"/mycreds.json:/app/src/mycreds.json \
+            ghcr.io/mkwant/list_to_sheets:latest "${arguments[@]}"
